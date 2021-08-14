3460afad4caa37c00eeddbb93be7258dd3170ee5
eno <k1990121@gmail.com>
2021-08-14 20:23:26 +0200

    haha

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -3,23 +3,21 @@
 int main(void)
 {
 	std::string cmd;
-	PhoneBook *book = new PhoneBook();
-	std::cout << std::right; 
-	std::cout.width(10);
+	PhoneBook book;
 	while (true)
 	{
 		std::cout << "1.ADD 2.SEARCH 3.EXIT" << std::endl;
  		std::cin >> cmd;
 		if (cmd.compare("ADD") == 0)
 		{
-			book->addContract();
+			book.addContract();
 		}
 		else if (cmd.compare("EXIT") == 0)
 			break ;
 		else if (cmd.compare("SEARCH") == 0)
 		{
-			book->searchContract();
-			book->detailContract();
+			book.searchContract();
+			book.detailContract();
 		}
 	}
 	return (0);
