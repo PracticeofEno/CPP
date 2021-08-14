f4b0aa9d4d3ba7d97a849aa0757ec54574193f2e
eno <k1990121@gmail.com>
2021-08-14 18:41:09 +0200

    haha

--- cpp00/ex00/main.cpp
+++ cpp00/ex00/main.cpp
@@ -34,16 +34,16 @@ int main(int argc, char **argv)
 			std::cout << str;
 			str.clear();
 		}
+		for (size_t j = 0; j < str.size(); j++)
+		{
+			str[j] = toupper(str[j]);
+		}
+		std::cout << str;
 	}
 	else
 	{
-		std::getline(std::cin, str);
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	}
-	for (size_t j = 0; j < str.size(); j++)
-	{
-		str[j] = toupper(str[j]);
-	}
-	std::cout << str;
 	std::cout << std::endl;
 	return (0);
 }
